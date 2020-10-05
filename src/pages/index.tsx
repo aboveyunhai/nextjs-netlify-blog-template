8f059dde487498926d7a7a5de5267e5648e3d4b6
aboveyunhai <35160613+aboveyunhai@users.noreply.github.com>
2020-10-05 15:45:26 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,9 @@ import { SocialList } from "../components/SocialList";
 export default function Index() {
   return (
     <Layout>
-      <Head>
+      <head>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-      </Head>
+      </head>
       <BasicMeta url={"/"} />
       <OpenGraphMeta url={"/"} />
       <TwitterCardMeta url={"/"} />
